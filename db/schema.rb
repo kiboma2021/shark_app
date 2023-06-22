625a743edc1a5531a17c94921c5539a3a95fba26
kiboma <omayioben@gmail.com>
2023-06-22 18:01:44 +0300

    Update the post partial form to depend on shark id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_21_042628) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_22_135613) do
+  create_table "posts", force: :cascade do |t|
+    t.text "comment"
+    t.integer "shark_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shark_id"], name: "index_posts_on_shark_id"
+  end
+
   create_table "sharks", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -18,4 +26,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_21_042628) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "sharks"
 end
