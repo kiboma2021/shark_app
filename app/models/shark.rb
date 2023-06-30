e012b45a4fa824237a73f5022d7377940aa1d983
kiboma <omayioben@gmail.com>
2023-06-30 06:57:22 +0300

    Add relationship between shark and comments

--- app/models/shark.rb
+++ app/models/shark.rb
@@ -1,5 +1,6 @@
 class Shark < ApplicationRecord
     has_many :posts, dependent: :destroy
+    has_many :comments, dependent: :destroy
     validates :name, presence:true, uniqueness:true
     validates :description, presence:true
 end
