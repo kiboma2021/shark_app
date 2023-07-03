b6ce610f2fbeba8d70aee1a02a9d5ed21d7ef832
kiboma <omayioben@gmail.com>
2023-07-03 07:37:05 +0300

    Update stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,8 @@
-import "controllers"
+//import "controllers"
+ 
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
